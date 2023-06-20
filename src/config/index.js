92e0a32dfad9284f22059427e45909242eeaf911
gaurav-ens <gaurav.yadav@ense.enterprises>
2023-06-20 23:14:27 +0530

    First commit

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    dbUrl: "mongodb+srv://tccapp22:DBpvzeEu5wV1ozbx@tccapp.gyhwt41.mongodb.net/tccapp?retryWrites=true&w=majority",
-    shopifyClientId: "556e6b063edf73d1222d5a5d2821a8e6",
-    shopifyClientSecret: "c97892cf83cba32743495c32fb95ce0e",
-    appUrl: "https://1b27-2409-40d0-bd-4801-9dff-baa1-ad98-ab44.ngrok-free.app"
-}
